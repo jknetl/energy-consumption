96e14b5c66fdbad247a1c4a67b41d97b74f412ab
Jakub Knetl <knetl.j@gmail.com>
2026-02-28 06:06:54 +0100

    Add generated code by Spring initializr

---
+++ src/main/java/com/github/jknetl/ec/EnergyConsumptionApplication.java
@@ -0,0 +1,13 @@
+package com.github.jknetl.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyConsumptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyConsumptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jknetl/ec/EnergyConsumptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jknetl.ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyConsumptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
