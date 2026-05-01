9e8ee81f524cfc6e7f044b4a14c09d351a0d7d07
Jakub Knetl <knetl.j@gmail.com>
2026-05-01 06:40:59 +0200

    Add missing annotation

--- src/main/java/com/github/jknetl/ec/data/repository/AppUserRepository.java
+++ src/main/java/com/github/jknetl/ec/data/repository/AppUserRepository.java
@@ -2,10 +2,12 @@ package com.github.jknetl.ec.data.repository;
 
 import com.github.jknetl.ec.data.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, UUID> {
     Optional<AppUser> findByEmail(String email);
 }
